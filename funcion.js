13c9df3f6d2e4ae49a91712daab8377ccf52c454
pappafabian17 <fabianpappa2000@gmail.com>
2022-06-04 20:05:34 -0300

    Final Change

--- funcion.js
+++ funcion.js
@@ -1,17 +1,8 @@
-/* let lista_nombre = [];
-let lista_monto = []; */
-
 function envio() {
   const nombreInput = document.getElementById("nombre");
   const montoInput = document.getElementById("monto");
 
   if (nombreInput.value != "" && montoInput.value != "") {
-    /* lista_nombre.push(nombreInput.value);
-    lista_monto.push(montoInput.value);
-
-    console.log(lista_nombre);
-    console.log(lista_monto);
- */
     sumaTotal(montoInput.value);
     addPeople(nombreInput.value, montoInput.value);
 
@@ -23,7 +14,7 @@ function envio() {
 }
 
 let montoTotal = 0;
-//hacer una funcion que agarre el elemento en el que tengo el total y hacerle un  innerHTML con el nuevo valor de la variable total. esta funcion va a recibir por parametro el ultimo monto ingresado para sumarselo a la variable total. esta funcion se va a ejecutar en addPeople.
+
 function sumaTotal(monto) {
   let totalTextContainer = document.getElementById("totalTextContainer");
   montoTotal = montoTotal + parseInt(monto);
@@ -33,18 +24,15 @@ function sumaTotal(monto) {
 let cantPeople = 0;
 
 const addPeople = (nombre, monto) => {
-  //arrow function con parametros
-  const resultados = document.getElementById("resultados"); // obtiene la referencia al elemento Html con el ID 'resultados' y lo guarda en una constante
-  let persona = document.createElement("p"); // crea un nuevo elemento Html eb el documento
-  const texto = nombre + ": $" + monto; // concatenamos los parametros
-  persona.innerHTML = texto; // anade texto al elemento persona, innerHtml sirve para eso
-  resultados.appendChild(persona); //incluye el elemento 'persona' dentro del elemento 'resultados'
+  const resultados = document.getElementById("resultados");
+  let persona = document.createElement("p");
+  const texto = nombre + ": $" + monto;
+  persona.innerHTML = texto;
+  resultados.appendChild(persona);
 
   cantPeople++;
 
   const aPagarCU = document.getElementById("aPagar");
   const montoAPagarCU = parseInt(montoTotal) / cantPeople;
   aPagarCU.innerHTML = "$" + montoAPagarCU;
-
-  //console.log(">>> CantPeople: ", cantPeople); imprimo para ver si suma las personas de cant people
 };
